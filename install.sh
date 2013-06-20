8d53e335cf9c593262c33c44041b62a618913ec2
Rasmey Sareth <rasmey.sareth@prologism.fr>
2013-06-20 13:34:41 +0200

    Downloading the pathogen plugin before installing the plugins.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Installing the pathogen plugin ..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
 echo "Downloading the plugins ..."
 if ! test -d "${HOME}"/.vim/bundle
 then
@@ -8,21 +13,21 @@ fi
 
 cd "${HOME}"/.vim/bundle
 echo "  -> NERDTree"
-git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdtree.git 1>/dev/null
 echo "  -> python-mode"
-git clone git://github.com/klen/python-mode.git
+git clone git://github.com/klen/python-mode.git 1>/dev/null
 echo "  -> vim-fugitive"
-git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-fugitive.git 1>/dev/null
 echo "  -> vim-powerline"
-git clone https://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/Lokaltog/vim-powerline.git 1>/dev/null
 echo "  -> vim-ruby"
-git clone git://github.com/vim-ruby/vim-ruby.git
+git clone git://github.com/vim-ruby/vim-ruby.git 1>/dev/null
 echo "  -> vim-scala"
-git clone https://github.com/derekwyatt/vim-scala.git
+git clone https://github.com/derekwyatt/vim-scala.git 1>/dev/null
 echo "  -> vim-scriptease"
-git clone https://github.com/tpope/vim-scriptease.git
+git clone https://github.com/tpope/vim-scriptease.git 1>/dev/null
 echo "  -> vim-sensible"
-git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/tpope/vim-sensible.git 1>/dev/null
 
 echo "Copying the vimrc file ..."
 mv "${HOME}"/.vimrc "${HOME}"/vimrc_backup
